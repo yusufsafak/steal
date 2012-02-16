5d41b782102fa8953518f2fe121ea40d1610b4b1
Ralph Holzmann <ralphholzmann@gmail.com>
2012-02-15 22:24:13 -0600

    Forgot to add config.js to the amd folder.

---
+++ amd/config.js
@@ -0,0 +1,18 @@
+
+//Map the configure({}) call to loader-specific call.
+var config = config,
+
+    //Map the top-level entry point to start loading to loader-specific call.
+    go = require,
+
+    //Indicate what levels of the API are implemented by this loader,
+    //and therefore which tests to run.
+    implemented = {
+        basic: true,
+        anon: true,
+        funcString: true,
+        namedWrapped: true,
+        require: true,
+        plugins: true,
+        pluginDynamic: true
+    };
