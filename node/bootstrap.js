0d358fcd563c1d5e222e7d46f5262f44f7216118
Yusuf Safak Bayram <yusufsafak@yahoo.com>
2012-04-22 00:58:16 +0300

    fixed some bugs

--- node/bootstrap.js
+++ node/bootstrap.js
@@ -1,14 +1,16 @@
-var fs = require('fs');
-
 global.steal = {
 	engine : "node",
 	
 	types : {
 		"js" : function(options, success){
 			try{
-				eval(options.text || fs.readFileSync(options.src, 'utf8'));
+				if(options.text){
+					eval(options.text);
+				}else{
+					require(options.src);
+				}
 			}catch(e){
-				//console.log(e);
+				console.log(e);
 				//console.log(e.stack);
 				//throw e;
 			}
@@ -21,3 +23,4 @@ require("steal/node/node.js");
 
 steal.args = process.argv.slice(2);
 steal.env = process.env;
+require(steal.args[0]);
